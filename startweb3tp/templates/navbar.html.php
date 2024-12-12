66a768a93a523f2111f50623c372ef70232a6024
Bartosz Wiaderny <10376794u@qed.local>
2024-12-12 16:43:16 +0100

    dod

---
+++ startweb3tp/templates/navbar.html.php
@@ -0,0 +1,12 @@
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+        <?php 
+             $_SESSION['user']['name'];
+        ?>
+        ewefwef
+    </a>
+    <a class="navbar-brand" href="#">Navbar</a>
+  </div>
+</nav>

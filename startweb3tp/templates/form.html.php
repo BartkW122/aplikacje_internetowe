a51ce39f7d10378a2b22e39ce9e2cd574b68c232
Bartosz Wiaderny <10376794u@qed.local>
2024-12-19 12:36:55 +0100

    dod

--- startweb3tp/templates/form.html.php
+++ startweb3tp/templates/form.html.php
@@ -1,6 +1,8 @@
 <?php
     $formAction = "index.php?page=index&action=";
     $formAction .= isset($_GET['id']) ? 'edit&id=' . $_GET['id'] : 'add';
+    echo "id: ".$_GET['id']."<br>";
+    echo "formaction: ".$formAction;
    /* if (isset($dbStatus['status']))
     {
         showMessage($dbStatus['status'], $dbStatus['msg']);

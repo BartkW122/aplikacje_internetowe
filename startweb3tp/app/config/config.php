f2c9db8dbc7be0db2eb262789269d7f2a92efc75
Bartosz Wiaderny <10376794u@qed.local>
2024-12-19 12:15:28 +0100

    poprawienie addresu

--- startweb3tp/app/config/config.php
+++ startweb3tp/app/config/config.php
@@ -7,6 +7,6 @@ if (isset($_SERVER['HTTPS']))
 }
 
 define ('APP_NAME', 'startweb');
-define ('PREFIX', '/startweb3tp/');
+define ('PREFIX', '/aplikacje_interentowe/startweb3tp/');
 define ('BASE_URL', $protocol . $_SERVER['HTTP_HOST'] . PREFIX);
 define ('PASS_SALT', 'xyz234@');

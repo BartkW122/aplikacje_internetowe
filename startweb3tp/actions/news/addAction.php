b69586d06635cba1e39a497e21d0ef6331afc18e
Bartek <bartoszwiadernylodz@gmail.com>
2025-01-22 22:21:56 +0100

    dodanie daty dodania aktualnosci do bazy danych'

--- startweb3tp/actions/news/addAction.php
+++ startweb3tp/actions/news/addAction.php
@@ -2,7 +2,7 @@
 
 if($_SERVER['REQUEST_METHOD'] == 'POST')
 {
-    fieldRequired('file', $_POST['plik']);
+    fieldRequired('file', $_FILES['plik']['name']);
 	fieldRequired('autor', $_POST['autor']);
 	fieldRequired('tytul', $_POST['name']);
     fieldRequired('tersc', $_POST['tresc']);
@@ -12,7 +12,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
 	{	
 		/* status Bool(true|false), msg String) */
 		$dbStatus = [];
-		
+		$data=date("d-m-Y");
 		$query = "INSERT INTO news SET news_filename = '{$_POST['plik']}', news_title = '{$_POST['name']}', news_author = '{$_POST['autor']}', news_content = '{$_POST['tresc']}'";
         if ($db->query($query))
         {
